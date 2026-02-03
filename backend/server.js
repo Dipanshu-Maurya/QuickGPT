1314c472fec9386f084a3ff1040833e58d00c553
Dipanshu-Maurya <dipanshumaurya2005@gmail.com>
2026-02-03 18:47:07 +0530

    make server & mongodb connection

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './configs/db.js'
+
+const app = express()
+
+connectDB()
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+//Routes
+app.get('/', (req,res)=> 
+    res.send('Server is Live!')
+)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on ${PORT}`)
+})
