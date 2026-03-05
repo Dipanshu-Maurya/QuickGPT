f9a5f55196d667bbcf514b76d6a1ca7483ba0fa6
Dipanshu-Maurya <dipanshumaurya2005@gmail.com>
2026-03-05 11:16:59 +0530

    correct typo issue

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -36,9 +36,9 @@ export const getChats = async (req, res) => {
 export const deleteChat = async (req, res) => {
     try {
         const userId = req.user._id
-        const {ChatId} = req.body
+        const {chatId} = req.body
 
-        await Chat.deleteOne({_id: ChatId, userId})
+        await Chat.deleteOne({_id: chatId, userId})
         
         res.json({ success: true, message: "Chat Deleted"})
     } catch (error) {
